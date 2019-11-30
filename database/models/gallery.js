dfc8fd0892d265593399d4449d868e9818e315e2
Temitope <tope_olajide@outlook.com>
2019-11-30 07:24:09 +0100

    Create Gallery Model

---
+++ database/models/gallery.js
@@ -0,0 +1,35 @@
+/** Define the Gallery database model/association
+ * @exports Gallery
+ * @param  {object} sequelize - sequelize
+ * @param  {object} DataTypes - sequelize Datatypes
+ * @return {object} The Gallery model
+ */
+module.exports = (sequelize, DataTypes) => {
+    const Gallery = sequelize.define('Gallery', {
+      imageId: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+          as: 'userId',
+        }
+      }
+    });
+    Gallery.associate = (models) => {
+      Gallery.belongsTo(models.User, {
+        foreignKey: 'userId',
+      });
+    };
+  
+    return Gallery;
+  };
+  
