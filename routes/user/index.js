75edab52918655ec5d1e05d906407b796886c03d
Temitope <tope_olajide@outlook.com>
2019-12-07 02:56:47 +0100

    Update User Route

--- routes/user/index.js
+++ routes/user/index.js
@@ -12,15 +12,22 @@ user.post('/signin', User.signIn);
 user.use('*', Auth.verify);
 user.put('/profile', User.modifyUser);
 user.put('/change-password', User.changePassword);
-user.get('/businesses', Businesses.getUserBusiness);  
-
-user
+user.get('/businesses', Businesses.getUserBusiness); 
+ user
   .route('/favourite/:businessId')
   .post(Favourite.addToFavourite)
-  .delete(Favourite.removeFromFavourites);
-user.get('/favourites', Favourite.getFavBusinesses);
+  .delete(Favourite.removeFromFavourites)
+user.get('/followee', Follower.fetchAllFollowees);
 
+user.get('/follower', Follower.fetchAllFollowers);
+
+user.get('/favourites', Favourite.getFavBusinesses);
 user
   .route('/follow/:userId')
   .post(Follower.followUser)
+  .delete(Follower.unFollowUser);
+
+
+
+
 export default user;
